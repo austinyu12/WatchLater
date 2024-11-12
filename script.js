e3eff498d002d567cf087ae7b94f236ce451a592
austinyu12 <ausno5@gmail.com>
2024-11-11 22:55:52 -0800

    experimenting with retrieving active url

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 1
 // function getLastActiveTabUrl() {
 //     return new Promise((resolve, reject) => {
 //         chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
@@ -10,22 +11,24 @@
 //     });
 // }
 
-async function getLastActiveTabUrl() {
-    // Use `chrome.tabs.query` to get the active tab in the current window
-    let tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    console.log("Tabs array returned: ", tabs);
-    let [lastActiveTab] = tabs;
-    // If there is a result, return the URL
-    if (lastActiveTab && lastActiveTab.url) {
-        return lastActiveTab.url;
-    } else {
-        throw new Error("No active tab found");
-    }
-}
+// 1.2
+// async function getLastActiveTabUrl() {
+//     // Use `chrome.tabs.query` to get the active tab in the current window
+//     let tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+//     console.log("Tabs array returned: ", tabs);
+//     let [lastActiveTab] = tabs;
+//     // If there is a result, return the URL
+//     if (lastActiveTab && lastActiveTab.url) {
+//         return lastActiveTab.url;
+//     } else {
+//         throw new Error("No active tab found");
+//     }
+// }
 
 function addToWatchLater() {
     console.log("button was clicked");
 
+    // 1
     // (async function() {
     //     try {
     //         const url = await getLastActiveTabUrl();
@@ -35,9 +38,17 @@ function addToWatchLater() {
     //     }
     // })();
 
-    getLastActiveTabUrl()
-    .then(url => console.log("Last active tab URL:", url))
-    .catch(error => console.error(error));
+    // 2
+    // getLastActiveTabUrl()
+    // .then(url => console.log("Last active tab URL:", url))
+    // .catch(error => console.error(error));
+
+    // 3
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //     var tab = tabs[0];
+    //     console.log(tab.url);
+    // })
+
 }
 
 function main() {
