7c6e5eb88623d9e15cfb02a89c4613ce0267aaa3
austinyu12 <ausno5@gmail.com>
2024-11-14 16:22:10 -0800

    got the code to grab the url working
    it was not a permissions issue, i tried using just one monitor and put some promising code back in that i had taken out

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
 // 1.2
 // async function getLastActiveTabUrl() {
 //     // Use `chrome.tabs.query` to get the active tab in the current window
-//     let tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+//     let tabs = await chrome.tabs.query({ : true });
 //     console.log("Tabs array returned: ", tabs);
 //     let [lastActiveTab] = tabs;
 //     // If there is a result, return the URL
@@ -24,6 +24,18 @@
 //         throw new Error("No active tab found");
 //     }
 // }
+
+function getLastActiveTabUrl() {
+    return new Promise((resolve, reject) => {
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
+            if (tabs.length > 0) {
+                resolve(tabs[0].url); // Resolve with the URL
+            } else {
+                reject("No active tab found.");
+            }
+        });
+    });
+}
 
 function addToWatchLater() {
     console.log("button was clicked");
@@ -39,15 +51,9 @@ function addToWatchLater() {
     // })();
 
     // 2
-    // getLastActiveTabUrl()
-    // .then(url => console.log("Last active tab URL:", url))
-    // .catch(error => console.error(error));
-
-    // 3
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //     var tab = tabs[0];
-    //     console.log(tab.url);
-    // })
+    getLastActiveTabUrl()
+    .then(url => console.log("Last active tab URL:", url))
+    .catch(error => console.error(error));
 
 }
 
