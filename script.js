6612363c245922091190e1137605b4009fab61ca
austinyu12 <ausno5@gmail.com>
2024-11-19 21:46:19 -0800

    implement primitive save and load playlist
    also refactored addToWatchLater

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 async function getLastActiveTabUrl() {
     let tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
-    //console.log("Tabs array returned: ", tabs);
     let [lastActiveTab] = tabs;
     if (lastActiveTab && lastActiveTab.url) {
         return lastActiveTab.url;
@@ -17,21 +16,62 @@ function createVideoObject(url) {
     playlist.appendChild(video);
 }
 
-function addToWatchLater() {
-    getLastActiveTabUrl()
-    .catch(error => {
-        console.error(error);
-        return;
-    })
-    .then(url => {
-        createVideoObject(url);
-    })
+/*
+When popup is unloaded, takes the playlist div and saves it to local storage.
+For some reason, Chrome Extensions does not diplay in Application Local Storage
+when the key:value pair is saved, so if no error messages return, it's a success.
+ */
+function savePlaylistToLocal() {
+    const playlist = document.querySelector('.playlist');
+    const playlistContent = playlist.innerHTML;
+    console.log(playlistContent);
+    if (chrome.storage && chrome.storage.local) {
+        //chrome.storage.local.set({ "key": "value" });
+        chrome.storage.local.set({ "playlistContent": playlistContent }, function () {
+            if (chrome.runtime.lastError) { 
+                console.error("Failed to save playlist:", chrome.runtime.lastError.message); 
+            } else { 
+                console.log("Playlist saved!");
+            }
+        });
+    } else {
+        console.error("Chrome storage could not be accessed");
+    }
+    
 }
 
+function loadPlaylistFromLocal() {
+    chrome.storage.local.get("playlistContent", function (result) {
+        if (result && result.playlistContent) {
+            console.log("successfully loaded");
+            console.log(result.playlistContent);
+            const playlistContent = document.querySelector('.playlist');
+            playlistContent.innerHTML = result.playlistContent;
+        }
+        else {
+            console.error("result or result.playlistContent not found");
+        }
+    });
+}
+
+function addToWatchLater() {
+    getLastActiveTabUrl()
+    .then(url => {
+        createVideoObject(url);
+        savePlaylistToLocal();
+    })
+    .catch(error => {
+        console.error(error);
+    })
+}
 
 function main() {
     const addButton = document.getElementById('add');
     addButton.addEventListener('click', addToWatchLater);
+    //function to load playlists on open
+    loadPlaylistFromLocal();
+    //create event listener to save playlists on close
+    //window.addEventListener('unload', savePlaylistToLocal);
 }
 
 document.addEventListener('DOMContentLoaded', main());
